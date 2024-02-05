2eb2b7ca5669100c08b2b8d8ad37393c7db5671b
Niels-1068212 <143613483+Niels-1068212@users.noreply.github.com>
2024-02-05 17:38:06 +0100

    home app

--- root/root/urls.py
+++ root/root/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('beheerder/', include('beheerder.urls'))
+    # path('beheerder/', include('beheerder.urls')),
+    path('', include('home.urls')),
 ]
