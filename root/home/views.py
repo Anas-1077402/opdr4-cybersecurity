f2935115dfd10591922845e1342e5902c17e9415
Niels-1068212 <143613483+Niels-1068212@users.noreply.github.com>
2024-02-06 13:21:37 +0100

    login html

--- root/home/views.py
+++ root/home/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, "home/index_test.html")
+
+
+def login(request):
+    return render(request, "home/login.html")
+
+def register(request):
+    return render(request, "home/register.html")
