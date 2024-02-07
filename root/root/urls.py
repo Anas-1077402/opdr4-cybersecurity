93d21ece350bd71d37b9e992725fe94f315bba23
Niels-1068212 <143613483+Niels-1068212@users.noreply.github.com>
2024-02-07 11:59:35 +0100

    added administrator app and new database

--- root/root/urls.py
+++ root/root/urls.py
@@ -19,6 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('beheerder/', include('beheerder.urls')),
     path('', include('home.urls')),
+    path('beheerder', include('administrator.urls'))
 ]
