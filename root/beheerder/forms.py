6ed73158662ed0dd7d793ca986fb97549c07435c
Niels-1068212 <143613483+Niels-1068212@users.noreply.github.com>
2024-02-10 09:13:15 +0100

    updated beheerder db and register

--- root/beheerder/forms.py
+++ root/beheerder/forms.py
@@ -3,9 +3,8 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
 class RegistratieFormulier(UserCreationForm):
-    email = forms.EmailField(max_length=200, help_text='Verplicht')
     functie = forms.CharField(max_length=100, help_text='Wat is je functie')
 
     class Meta:
         model = CustomUser
-        fields = ('username', 'email', 'password1', 'password2', 'functie')
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2', 'functie')
--- root/beheerder/models.py
+++ root/beheerder/models.py
@@ -5,4 +5,4 @@ from django.db import models
 class CustomUser(AbstractUser):
     # hier later extra vakken toevoegen...
     functie = models.CharField(max_length=100, blank=True, null=True)
-
+    status = models.IntegerField(default=1)
