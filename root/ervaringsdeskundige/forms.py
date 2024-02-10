b76da475a8dd92819b25a7a611a0b53de67b2330
Anas-1077402 <145554176+Anas-1077402@users.noreply.github.com>
2024-02-10 15:16:30 +0100

    velden toegevoegd die nodig zijn models.py

--- root/ervaringsdeskundige/forms.py
+++ root/ervaringsdeskundige/forms.py
@@ -1,8 +1,9 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from .models import Ervaringsdeskundige
 
 
-class RegisterForm(forms.ModelForm):
+class RegisterForm(UserCreationForm):
     class Meta:
         model = Ervaringsdeskundige
-        fields = ['e-mail', 'wachtwoord', 'gebruikersnaam', 'voornaam', 'achternaam', 'postcode', 'geslacht', 'gebruikte_hulpmiddelen', 'geboorte_datum', 'bijzonderheden', 'voogd', 'voornaam_voogd', 'achternaam_voogd', 'e-mail_voogd', 'voorkeur_benadering', 'telefonisch_onderzoek', 'internet_onderzoek', 'locatie_onderzoek', 'bijzonderheden_beschikbaarheid']
+        fields = ('email', 'wachtwoord', 'gebruikersnaam', 'voornaam', 'achternaam', 'postcode', 'geslacht', 'gebruikte_hulpmiddelen', 'geboorte_datum', 'bijzonderheden', 'voogd', 'voornaam_voogd', 'achternaam_voogd', 'email_voogd', 'voorkeur_benadering', 'telefonisch_onderzoek', 'internet_onderzoek', 'locatie_onderzoek', 'bijzonderheden_beschikbaarheid')
--- root/ervaringsdeskundige/models.py
+++ root/ervaringsdeskundige/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class Ervaringsdeskundige(AbstractUser):
@@ -10,5 +10,15 @@ class Ervaringsdeskundige(AbstractUser):
     achternaam = models.CharField(max_length=100)
     postcode = models.CharField(max_length=10)
     geslacht = models.CharField(max_length=10, choices=[('M','Man'),('V', 'Vrouw'), ('0', 'Anders')])
-    geboortedatum = models.DateField()
+    geboorte_datum = models.DateField()
+    gebruikte_hulpmiddelen = models.CharField(max_length=100)
+    bijzonderheden = models.CharField(max_length=100)
+    voogd = models.CharField(max_length=100)
+    voornaam_voogd = models.CharField(max_length=100)
+    email_voogd = models.CharField(max_length=100)
+    voorkeurbenadering = models.CharField(max_length=100)
+    telefonisch_onderzoek = models.CharField(max_length=100)
+    internet_onderzoek = models.CharField(max_length=100)
+    locatie_onderzoek = models.CharField(max_length=100)
+    bijzonderheden_beschikbaarheid = models.CharField(max_length=100)
     #hier ga ik nog meer velden toevoegen
